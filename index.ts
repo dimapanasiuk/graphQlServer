c6b88746d1c684afd52c76e6606160c85ae11632
dimapanasiuk <dimapanasiuk@icloud.com>
2021-01-27 23:30:20 +0300

    feat: add eslint

--- index.ts
+++ index.ts
@@ -1,35 +1,43 @@
-import express from 'express';
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./scheme');
-const cors = require('cors');
+import express from "express";
+
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./scheme");
+const cors = require("cors");
 
 const app = express();
-const PORT: number = 5000;
-const basicStr: string = 'Node TS app!'
 
+const PORT = 5000;
 
-const users: any[] = [{ id: 1, username: 'Masonovv', age: 25 }];
+const basicStr = "Node TS app!";
+
+const users: any[] = [{ id: 1, username: "Masonovv", age: 25 }];
+
 
 const root = {
-  getAllUsers: () => {
-    return users;
-  },
+
+  getAllUsers: () =>  users,
   getUser: ({ id }: any): any => {
-    return users.find(user => user.id === id)
+    return users.find(user => user.id === id);
   }
-}
+};
 
 app.use(cors());
-app.use('/graphql', graphqlHTTP({
-  graphiql: true,
-  schema,
-  rootValue: root
-}));
+
+app.use("/graphql", graphqlHTTP(
+  {
+    graphiql: true,
+    schema,
+    rootValue: root
+  }
+));
 
 
+app.get("/", (req, res) => {
 
-app.get('/', (req, res) => {
   res.send(basicStr);
+
 });
 
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
