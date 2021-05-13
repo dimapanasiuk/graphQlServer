24bbbc4a093be557d628c4f4d00184e768a1c76c
panakota <dimapanasiuk@icloud.com>
2021-05-13 18:55:12 +0300

    feat: add mongodb to project

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-
+// const mongoose = require("mongoose")
 const passport = require("passport");
 const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
@@ -12,6 +12,31 @@ const { root }  =require("./scheme/root");
 const PORT = 5000;
 
 const app = express();
+
+
+async function main() {
+  const MongoClient = require("mongodb").MongoClient;
+  const uri ="mongodb+srv://Dima:Dima1995@cluster0.vyymm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+  });
+
+  try {
+    client.connect((err: any) => {
+      const collection = client.db("test").collection("devices");
+      console.log({collection});
+      client.close();
+    });
+    console.log("db connect");
+  } catch (e) {
+    console.log("server error", e.message);
+    client.close(1);
+  }
+
+
+}
+main().catch(console.error);
+
 
 app.use(unHandledErrorMiddleware); //for all endpoints which dosen't have try catch
 app.use(cors());
